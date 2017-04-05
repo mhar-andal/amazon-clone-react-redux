ea7b3f4d43c9b09f43c2bf4c422f5493dc6f18a1
rrichardsonv <rrichardsonv+github@gmail.com>
2017-04-05 14:38:45 -0500

    Added heading and list sections

--- js/Sidebar.js
+++ js/Sidebar.js
@@ -11,7 +11,17 @@ class Sidebar extends Component {
           </div>
           <div className='filter-section'>
             <div className='inner-wrapper'>
-              <p>asdoih oa sdgoias dgioads gioasd gio adsoiha dsgios dagioh asgoihds agoi dsadghio sdgoihds goih sdgiohs daghio dgsaoih dsagoih dsaghio </p>
+              <ul>
+                <li>
+                  <div className='rating-slider'>
+                    <h4 className='slide-header'>Rating</h4>
+                    <span>Adjust Rating</span>
+                    <div className='col-sm-12'>
+                      <div className='slider' />
+                    </div>
+                  </div>
+                </li>
+              </ul>
             </div>
           </div>
           <div className='filter-section'>
