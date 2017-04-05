a8b538f7b799f6b0fbe3b85651baa04fed7f382e
rrichardsonv <rrichardsonv+github@gmail.com>
2017-04-05 15:30:46 -0500

    moved slider logic to its own component commented out redux related things

--- js/Sidebar.js
+++ js/Sidebar.js
@@ -1,21 +1,17 @@
 import React, { Component, PropTypes } from 'react'
+import RangeFilter from './RangeFilter'
 import { connect } from 'react-redux'
-import { Row, Col } from 'react-bootstrap'
 import './Sidebar.css'
 
 class Sidebar extends Component {
   constructor (props) {
     super(props)
+    // this.handleFilterChange = this.handleFilterChange.bind(this)
+    // this.state = {
+    //   filter: ''
+    // }
+  }
 
-    this.handleFilterChange = this.handleFilterChange.bind(this)
-    this.state = {
-      filter: ''
-    }
-  }
-  handleFilterChange (event) {
-    // this.props.dispatch(event.target.value)
-    this.setState({ filter: event.target.value })
-  }
   render () {
     return (
       <div className='sidebar'>
@@ -23,49 +19,17 @@ class Sidebar extends Component {
           <div role='heading' className='sidebar-header'>
             Refine Search
           </div>
-          <div className='filter-section'>
-            <div className='container-fluid'>
-              <Row className='inner-wrapper'>
-                <Col className='Ratings' sm={12} md={12}>
-                  <div role='subheading' className='slide-header'>
-                    <strong>Minimum Rating: </strong>
-                    <span>{this.state.filter}</span>
-                  </div>
-                </Col>
-              </Row>
-              <Row className='slide-wrapper'>
-                <input
-                  min='0'
-                  max='5'
-                  onChange={this.handleFilterChange}
-                  value={this.state.filter}
-                  className='col-md-12'
-                  type='range'
-                />
-              </Row>
-            </div>
-          </div>
-          <div className='filter-section'>
-            <div className='inner-wrapper'>
-              <p>asdoih oa sdgoias dgioads gioasd gio adsoiha dsgios dagioh asgoihds agoi dsadghio sdgoihds goih sdgiohs daghio dgsaoih dsagoih dsaghio </p>
-            </div>
-          </div>
+          <RangeFilter />
         </div>
       </div>
     )
   }
 }
-const { func, string } = PropTypes
 
-Sidebar.propTypes = {
-  dispatch: func,
-  filter: string
-}
-
-const mapStateToProps = (state) => {
-  return {
-    filter: state.filter
-  }
-}
-
-export default connect(mapStateToProps)(Sidebar)
+// const mapStateToProps = (state) => {
+//   return {
+//     filter: state.filter
+//   }
+// }
+export default Sidebar
+// export default connect(mapStateToProps)(Sidebar)
