3da900a90b0d2d75564e3818c158ee4f9faa9970
Mhar Andal <mharandal96@gmail.com>
2017-04-05 12:36:30 -0500

    solved BUG(keys in filter and map error) finished Item card skeleton moving on to styling

--- js/Landing.js
+++ js/Landing.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import NavbarHead from './Navbar'
+import Content from './Content'
 
 class Landing extends Component {
   render () {
     return (
       <div>
         <NavbarHead />
+        <Content />
       </div>
     )
   }
